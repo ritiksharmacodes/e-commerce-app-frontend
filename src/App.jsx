3440dd966a1535357a130a6de85314a31d3ec5ab
ritiksharmacodes <ritikcan18@gmail.com>
2025-07-20 10:34:40 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react'
 import { Navbar } from './components'
+import { useEffect } from 'react'
+
+const addressOfTheServer = 'http://localhost:5050';
 
 function App() {
+  useEffect(() => {
+    fetch(`${addressOfTheServer}/api/v1/products/99999`).then((res) => res.json()).then((result) => console.log(result[0].extra_details_object["Camera And Display Features"]));
+  }, []);
 
   return (
     <>
