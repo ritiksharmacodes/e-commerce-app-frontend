ed4f29b80991d6149d4842d202a55ebcc60aeefe
ritiksharmacodes <ritikcan18@gmail.com>
2025-07-21 23:55:35 +0530

    minor changes

--- src/pages/ProductDisplayPage.jsx
+++ src/pages/ProductDisplayPage.jsx
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react'
 import Navbar from "../components/Navbar.jsx";
 import Carousel from '../components/Carousel.jsx';
 
+
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
 const addressOfTheServer = 'http://localhost:5050';
 
 function ProductDisplayPage() {
@@ -18,12 +26,68 @@ function ProductDisplayPage() {
             }
         })();
     }, []);
+    console.log(data[0]);
+
+
+    // below code is for the price formatter
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'INR',
+
+        // These options can be used to round to whole numbers.
+        trailingZeroDisplay: 'stripIfInteger'   // This is probably what most people
+        // want. It will only stop printing
+        // the fraction when the input
+        // amount is a round number (int)
+        // already. If that's not what you
+        // need, have a look at the options
+        // below.
+        //minimumFractionDigits: 0, // This suffices for whole numbers, but will
+        // print 2500.10 as $2,500.1
+        //maximumFractionDigits: 0, // Causes 2500.99 to be printed as $2,501
+    });
 
     return (
-        <div>
-            <Navbar />
-            <Carousel data={data} />
-        </div>
+        <>
+            <div className=''>
+                <Navbar />
+                <Carousel data={data} />
+
+                <div className='flex justify-center items-center gap-2 p-2'>
+                    <button className='bg-blue-400 px-5 py-2 rounded-lg text-white capitalize shadow-lg cursor-pointer'>Add to cart</button>
+                    <button className='capitalize bg-amber-300 px-5 py-2 rounded-lg shadow-lg cursor-pointer'>Buy now</button>
+                </div>
+
+                <p className='p-2 mt-4'>{data[0]?.name}</p>
+
+                <p className='flex items-center p-2 text-2xl font-bold'>{formatter.format(data[0]?.mrp)}</p>
+
+                <div className='p-2 mt-4'>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ArrowDownwardIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            <Typography component="span">Product Details</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
+
+                <div className='bg-green-900 h-[150px]'></div>
+                <div className='bg-purple-900 h-[150px]'></div>
+                <div className='bg-pink-900 h-[150px]'></div>
+
+            </div>
+
+            <div className='text-blue-800 text-center h-[150px]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore ex quidem nemo nam! Quae nulla rerum quas eius ducimus iste quis reprehenderit, id beatae mollitia consectetur a recusandae labore minima.</div>
+        </>
     )
 }
 
