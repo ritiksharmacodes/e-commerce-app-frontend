eab3c554ce54fedc0764da13bd6414730beea082
ritiksharmacodes <ritikcan18@gmail.com>
2025-07-26 11:07:39 +0530

    minor changes

--- src/components/features/StillLookingForThese.jsx
+++ src/components/features/StillLookingForThese.jsx
@@ -11,12 +11,12 @@ function StillLookingForThese() {
             <h1 className='font-bold'>Still looking for these?</h1>
             <Swiper
                 slidesPerView={3}
-                spaceBetween={30}
+                spaceBetween={110}
                 className='mt-4'
                 loop={true}
             >
                 <SwiperSlide>
-                    <div className='rounded-2xl bg-white p-2 min-h-[120px] w-fit'>
+                    <div className='rounded-2xl bg-white p-2 min-h-[120px] min-w-[119px] max-w-fit'>
                         <div>
                             <img className='h-[90px] m-auto rounded-md' src="https://rukminim2.flixcart.com/image/390/390/xif0q/smartwatch/s/i/u/-original-imah76jt64ffmwg4.jpeg?q=80" alt="" />
                         </div>
@@ -24,7 +24,7 @@ function StillLookingForThese() {
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className='rounded-2xl bg-white p-2 min-h-[120px] w-fit'>
+                    <div className='rounded-2xl bg-white p-2 min-h-[120px] min-w-[119px] max-w-fit'>
                         <div>
                             <img className='h-[90px] m-auto rounded-md' src="https://rukminim2.flixcart.com/image/390/390/xif0q/mobile/s/v/h/-original-imahd57g62dfepkh.jpeg?q=80" alt="" />
                         </div>
@@ -32,7 +32,7 @@ function StillLookingForThese() {
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className='rounded-2xl bg-white p-2 min-h-[120px] w-fit'>
+                    <div className='rounded-2xl bg-white p-2 min-h-[120px] min-w-[119px] max-w-fit'>
                         <div>
                             <img className='h-[90px] m-auto rounded-md' src="https://rukminim2.flixcart.com/image/734/1102/xif0q/face-wash/5/1/k/100-ocean-for-men-fights-acne-pimple-skin-brightening-all-skin-original-imahajnft9f9hfjc.jpeg?q=90&crop=false" alt="" />
                         </div>
@@ -40,7 +40,7 @@ function StillLookingForThese() {
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className='rounded-2xl bg-white p-2 min-h-[120px] w-fit'>
+                    <div className='rounded-2xl bg-white p-2 min-h-[120px] min-w-[119px] max-w-fit'>
                         <div>
                             <img className='h-[90px] m-auto rounded-md' src="https://rukminim2.flixcart.com/image/734/880/xif0q/pressure-cooker/h/q/w/yes-3-pearlspecialcombo235l-greenchef-original-imah5ezn3zyhk8qs.jpeg?q=90&crop=false" alt="" />
                         </div>
