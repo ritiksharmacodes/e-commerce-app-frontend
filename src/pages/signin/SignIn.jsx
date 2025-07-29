be59ce3814cd929a5e538f7fc08e474e663eb38d
ritiksharmacodes <ritikcan18@gmail.com>
2025-07-29 11:15:45 +0530

    minor changes

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-import {  Link } from "react-router-dom";
-import BasicForm from './BasicForm';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { FaLessThanEqual, FaRegEye } from "react-icons/fa6";
+import { FaRegEyeSlash } from "react-icons/fa6";
+import { FaGoogle } from "react-icons/fa";
+import { useGoogleLogin } from '@react-oauth/google';
+
+
 
 function SignIn() {
+    const { register, handleSubmit } = useForm();
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const googleOauthLogin = useGoogleLogin({
+        onSuccess: tokenResponse => console.log(tokenResponse),
+    });
+
     return (
         <div className=''>
 
@@ -16,7 +28,26 @@ function SignIn() {
                     <p className='capitalize inline-block mr-1'>don't have an account?</p>
                     <Link className='font-bold underline' to={'/signup'}>Create an account </Link>
                 </div>
-                <BasicForm />
+
+                <form className='flex flex-col gap-7'>
+                    <div className=''>
+                        <input className='p-4 rounded-lg border-1 w-full' type="email" placeholder='Email' />
+                    </div>
+                    <div className='relative'>
+                        <input className='p-4 rounded-lg border-1 w-full' type={`${!isPasswordVisible ? "password" : "text"}`} placeholder='Password' />
+                        {!isPasswordVisible ? <FaRegEye onClick={() => setIsPasswordVisible(true)} className='text-2xl absolute right-0 top-[50%] translate-[-50%]' /> : <FaRegEyeSlash onClick={() => setIsPasswordVisible(false)} className='text-2xl absolute right-0 top-[50%] translate-[-50%]' />}
+                    </div>
+                    <button className='bg-blue-400 rounded-md text-white font-semibold py-3' type="submit">Sign In</button>
+                    <div className='py-1 relative'>
+                        <hr className='text-gray-500' />
+                        <p className='absolute px-4 bg-white top-1/2 left-1/2 -translate-1/2 text-nowrap font-bold'>or continue with</p>
+                    </div>
+                </form>
+
+                <div onClick={() => googleOauthLogin()} className='rounded-md text-black font-semibold py-3 border-1 flex justify-center items-center gap-2'>
+                    <FaGoogle />
+                    <p>Google</p>
+                </div>
             </div>
 
         </div>
